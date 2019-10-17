e330b99d0ab8b3aee653218c1f80a80f655b2ece
capt-alien <eric.botcher@presidio.edu>
2019-10-17 14:57:12 -0700

    users can now create an author

--- public/src/app/new/new.component.ts
+++ public/src/app/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  newAuthor = {
+     // EX: book: "",
+        name: ''
+  };
+  errors;
 
-  constructor() { }
+  constructor(private http: HttpService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  createAuthor(){
+    console.log("NEw Button Clicked")
+    const observable = this.http.postAuthor(this.newAuthor);
+    observable.subscribe((data: any)=>{
+      if(data.message === 'fail'){
+        this.errors = data.errors;
+      } else{
+        this.router.navigate(['/']);
+      }
+    })
   }
 
 }
